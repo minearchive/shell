708f3e0da6ea01345e0a57ea7c9a5fd679e326b3
minearchive <102400088+minearchive@users.noreply.github.com>
2026-08-21 09:23:48 +0900

    chore: renaming deprecated dependency

--- devenv.nix
+++ devenv.nix
@@ -27,6 +27,6 @@
   ];
 
   env.LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-  env.LD_LIBRARY_PATH = "${pkgs.mesa.drivers}/lib:${pkgs.libGL}/lib";
+  env.LD_LIBRARY_PATH = "${pkgs.mesa}/lib:${pkgs.libGL}/lib";
   env.__EGL_VENDOR_LIBRARY_DIRS = "/run/opengl-driver/share/glvnd/egl_vendor.d";
 }
